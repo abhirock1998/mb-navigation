fb5d30ca6d739133e5df0d025119fddb7c2b3874
Jack1998 <yaduvanshiabhi1998@gmail.com>
2023-01-25 19:30:05 +0530

    feat: add dist folder for distribution

---
+++ dist/typings.d.ts
@@ -0,0 +1,44 @@
+/** @type {[number, number]}
+ * Provide an array with longitude and latitude [$longitude, $latitude]
+ */
+
+declare type Coordinate = [number, number];
+
+declare type onDestinationArrival = {
+  nativeEvent?: {
+    destinationLatitude: number;
+    destinationLongitude: number;
+  };
+};
+
+declare type onNavigationCancelled = {
+  nativeEvent?: {
+    message?: string;
+  };
+};
+
+declare type onEvent = {
+  nativeEvent?: {
+    message?: string;
+  };
+};
+
+declare type onError = {
+  nativeEvent?: {
+    message?: string;
+  };
+};
+
+export interface IMapboxNavigationProps {
+  origin?: Coordinate;
+  destination?: Coordinate;
+  shouldSimulateRoute?: boolean;
+  onError?: (event: onError) => void;
+  onEvent?: (event: onEvent) => void;
+  onNavigationCancel?: (event: onNavigationCancelled) => void;
+  onArrive?: (event: onDestinationArrival) => void;
+  onDestinationArrival: (event: onDestinationArrival) => void;
+  showsEndOfRouteFeedback?: boolean;
+  hideStatusView?: boolean;
+  mute?: boolean;
+}
---
+++ src/typings.ts
@@ -0,0 +1,46 @@
+/** @type {[number, number]}
+ * Provide an array with longitude and latitude [$longitude, $latitude]
+ */
+
+type Coordinate = [number, number];
+
+type onDestinationArrival = {
+  nativeEvent?: {
+    destinationLatitude: number;
+    destinationLongitude: number;
+  };
+};
+
+type onNavigationCancelled = {
+  nativeEvent?: {
+    message?: string;
+  };
+};
+
+type onEvent = {
+  nativeEvent?: {
+    message?: string;
+  };
+};
+
+type onError = {
+  nativeEvent?: {
+    message?: string;
+  };
+};
+
+export interface IMapboxNavigationProps {
+  origin?: Coordinate;
+  destination?: Coordinate;
+  shouldSimulateRoute?: boolean;
+  onError?: (event: onError) => void;
+  onEvent?: (event: onEvent) => void;
+  onNavigationCancel?: (event: onNavigationCancelled) => void;
+  onArrive?: (event: onDestinationArrival) => void;
+  onDestinationArrival: (event: onDestinationArrival) => void;
+  showsEndOfRouteFeedback?: boolean;
+  hideStatusView?: boolean;
+  mute?: boolean;
+}
+
+export {};
